019298a026b3101fa562bfb139edd41620f0cf07
bouzaghrane <amine.bouzaghrane@berkeley.edu>
2022-12-04 22:31:57 +0000

    modify mortality model restructuring func

--- demos_urbansim/models.py
+++ demos_urbansim/models.py
@@ -268,100 +268,69 @@ def remove_dead_persons(persons, households, fatality_list, year):
 
     persons_df["dead"] = fatality_list
     graveyard = persons_df[persons_df["dead"] == 1].copy()
-    # print(persons_df["household_id"].unique().shape[0])
-    # print(houses.index.unique().shape[0])
+
     #################################
     # HOUSEHOLD WHERE EVERYONE DIES #
     #################################
     # Get households where everyone died
     persons_df["member"] = 1
+    
     dead_frac = persons_df.groupby("household_id").agg(
-        num_dead=("dead", "sum"), size=("member", "sum")
+        num_dead=("dead", "sum"),
+        size=("member", "sum")
     )
-    dead_households = dead_frac[
-        dead_frac["num_dead"] == dead_frac["size"]
-    ].index.to_list()
+    
+    DEAD_HH_COND = dead_frac["num_dead"] == dead_frac["size"]
+    dead_households_ids = dead_frac[DEAD_HH_COND].index.to_list()
 
-    grave_households = houses[houses.index.isin(dead_households)].copy()
-    grave_persons = persons_df[persons_df["household_id"].isin(dead_households)].copy()
+    grave_households = houses[houses.index.isin(dead_households_ids)].copy()
+    grave_persons = persons_df[persons_df["household_id"].isin(dead_households_ids)].copy()
 
-    # Drop out of the persons table
-    persons_df = persons_df.loc[~persons_df["household_id"].isin(dead_households)]
     # Drop out of the households table
-    houses = houses.drop(dead_households)
-
+    houses = houses.drop(dead_households_ids)
+    # Drop out of the persons table
+    persons_df = persons_df.loc[~persons_df["household_id"].isin(dead_households_ids)]
+    
     ##################################################
     ##### HOUSEHOLDS WHERE PART OF HOUSEHOLD DIES ####
     ##################################################
-    dead = persons_df[persons_df["dead"] == 1].copy()
-    alive = persons_df[persons_df["dead"] == 0].copy()
-    # print("Finished splitting dead and alive")
+    dead_persons = persons_df[persons_df["dead"] == 1].copy()
+    alive_persons = persons_df[persons_df["dead"] == 0].copy()
 
     #################################
     # Alive heads, Dead partners
     #################################
     # Dead partners, either married or cohabitating
-    dead_partners = dead[dead["relate"].isin([1, 13])]  # This will need changed
-    # Alive heads
-    alive_heads = alive[alive["relate"] == 0]
-    alive_heads = alive_heads[["household_id", "MAR"]]
-    widow_heads = alive_heads.reset_index().merge(
-        dead_partners[["household_id"]], how="inner", on="household_id"
-    )
-    widow_heads = widow_heads.set_index("person_id")
-    widow_heads["MAR"].values[:] = 3
+    alive_heads = alive_persons[alive_persons["relate"] == 0]
+    dead_heads = dead_persons[dead_persons["relate"] == 0]
+    alive_partners = alive_persons[alive_persons["relate"].isin([1, 13])]
+    dead_partners = dead_persons[dead_persons["relate"].isin([1, 13])]
 
-    # Dead heads, alive partners
-    dead_heads = dead[dead["relate"] == 0]
-    alive_partner = alive[alive["relate"].isin([1, 13])]
-    alive_partner = alive_partner[
-        ["household_id", "MAR"]
-    ]  ## Pull the relate status and update it
-    widow_partners = alive_partner.reset_index().merge(
-        dead_heads[["household_id"]], how="inner", on="household_id"
-    )
-    widow_partners = widow_partners.set_index("person_id")
-    widow_partners["MAR"].values[:] = 3  # THIS MIGHT NEED VERIFICATION, WHAT DOES MAR MEAN?
-
+    widow_heads = get_widows(alive_heads, dead_partners)
+    widow_partners = get_widows(alive_partners, dead_heads)
+    
     # Merge the two groups of widows
     widows = pd.concat([widow_heads, widow_partners])[["MAR"]]
+    
     # Update the alive database's MAR values using the widows table
-    alive = widows.combine_first(alive)
-    alive["MAR"] = alive["MAR"].astype(int)
+    alive_persons = widows.combine_first(alive_persons)
+    alive_persons["MAR"] = alive_persons["MAR"].astype(int)
     # print("Finished updating marital status")
 
     # Select the households in alive where the heads died
-    alive_sort = (
-        alive.reset_index()
-        .merge(dead_heads[["household_id"]], how="inner", on="household_id")
-        .set_index("person_id")
-    )
-    alive_sort["relate"] = alive_sort["relate"].astype(int)
+    hh_dead_head = dead_heads["household_id"].unique()
+    alive_persons = reorganize_households_dead_head(alive_persons, hh_dead_head)
+    
+    alive_persons["person"] = 1
+    alive_persons["is_head"] = np.where(alive_persons["relate"] == 0, 1, 0)
+    alive_persons["race_head"] = alive_persons["is_head"] * alive_persons["race_id"]
+    alive_persons["age_head"] = alive_persons["is_head"] * alive_persons["age"]
+    alive_persons["hispanic_head"] = alive_persons["is_head"] * alive_persons["hispanic"]
+    alive_persons["child"] = np.where(alive_persons["relate"].isin([2, 3, 4, 14]), 1, 0)
+    alive_persons["senior"] = np.where(alive_persons["age"] >= 65, 1, 0)
+    alive_persons["age_gt55"] = np.where(alive_persons["age"] >= 55, 1, 0)
 
-    if len(alive_sort.index) > 0:
-        alive_sort.sort_values("relate", inplace=True)
-        # Restructure all the households where the head died
-        alive_sort = alive_sort[["household_id", "relate", "age"]]
-        # print("Starting to restructure household")
-
-        # Apply the rez function
-        alive_sort = alive_sort.groupby("household_id").apply(rez)
-
-        # Update relationship values and make sure correct datatype is used
-        alive = alive_sort[["relate"]].combine_first(alive)
-        alive["relate"] = alive["relate"].astype(int)
-        # print("Finished restructuring households")
-
-    alive["person"] = 1
-    alive["is_head"] = np.where(alive["relate"] == 0, 1, 0)
-    alive["race_head"] = alive["is_head"] * alive["race_id"]
-    alive["age_head"] = alive["is_head"] * alive["age"]
-    alive["hispanic_head"] = alive["is_head"] * alive["hispanic"]
-    alive["child"] = np.where(alive["relate"].isin([2, 3, 4, 14]), 1, 0)
-    alive["senior"] = np.where(alive["age"] >= 65, 1, 0)
-    alive["age_gt55"] = np.where(alive["age"] >= 55, 1, 0)
-
-    households_new = alive.groupby("household_id").agg(
+    households_new = alive_persons.groupby("household_id").agg(
         income=("earning", "sum"),
         race_of_head=("race_head", "sum"),
         age_of_head=("age_head", "sum"),
@@ -400,6 +369,7 @@ def remove_dead_persons(persons, households, fatality_list, year):
             ),
         ),
     )
+    
     households_new["hh_workers"] = np.where(
         households_new["workers"] == 0,
         "none",
@@ -428,17 +398,6 @@ def remove_dead_persons(persons, households, fatality_list, year):
 
     houses.update(households_new)
 
-    # print("Updating age stats table")
-    # Get the age over time table populated
-    age_over_time = orca.get_table("age_over_time").to_frame()
-    if age_over_time.empty:
-        age_over_time = pd.DataFrame([alive["person_age"].value_counts()])
-    else:
-        new_age_over_time = pd.DataFrame([alive["person_age"].value_counts()])
-        age_over_time = pd.concat([age_over_time, new_age_over_time], ignore_index=True)
-    orca.add_table("age_over_time", age_over_time)
-
-    # print("Update the population stats over time.")
     # Update the population over time stats
     graveyard_table = orca.get_table("pop_over_time").to_frame()
     if graveyard_table.empty:
@@ -447,27 +406,62 @@ def remove_dead_persons(persons, households, fatality_list, year):
     else:
         dead_people = pd.concat([graveyard_table, grave_persons])
 
-    # print("Update the dead households and graveyard.")
-    # Load the dead households and graveyard table
-    # Update persons table
-    orca.add_table("persons", alive[persons_columns])
+    # Update the orca tables
+    orca.add_table("persons", alive_persons[persons_columns])
     orca.add_table("households", houses[households_columns])
     orca.add_table("graveyard", dead_people[persons_columns])
-    # orca.add_injectable(
-    #     "max_p_id", orca.get_injectable("max_p_id"), alive["household_id"].max()
-    # )
-    metadata = orca.get_table("metadata").to_frame()
-    max_hh_id = metadata.loc["max_hh_id", "value"]
-    max_p_id = metadata.loc["max_p_id", "value"]
-    persons_df = orca.get_table("persons").local
-    households_df = orca.get_table("households").local
-    if households_df.index.max() > max_hh_id:
-        metadata.loc["max_hh_id", "value"] = households_df.index.max()
-    if persons_df.index.max() > max_p_id:
-        metadata.loc["max_p_id", "value"] = persons_df.index.max()
-    orca.add_table("metadata", metadata)
-    # print("DONE updating persons.")
 
+    update_max_id_metadata()
+
+
+def reorganize_households_dead_head(alive_persons, hh_dead_head):
+    """Function that identifies households with a dead head and
+    reorganizes households functions in the household.
+
+    Args:
+        alive_persons (pd.DataFrame): DataFrame of the persons alive
+        hh_dead_head (array): Array of household ids with dead head
+
+    Returns:
+        pd.DataFrame: Updated DataFrame of alive persons with new
+        households roles
+    """
+    alive_p_dead_hh_head = alive_persons[alive_persons["household_id"].isin(hh_dead_head)]
+    alive_p_dead_hh_head["relate"] = alive_p_dead_hh_head["relate"].astype(int)
+
+    if alive_p_dead_hh_head.shape[0] > 0:
+        alive_p_dead_hh_head.sort_values("relate", inplace=True)
+        # Restructure all the households where the head died
+        alive_p_dead_hh_head = alive_p_dead_hh_head[["household_id", "relate", "age"]]
+        # print("Starting to restructure household")
+
+        # Apply the rez function
+        alive_p_dead_hh_head = alive_p_dead_hh_head.groupby("household_id").apply(rez)
+
+        # Update relationship values and make sure correct datatype is used
+        alive_persons = alive_p_dead_hh_head[["relate"]].combine_first(alive_persons)
+        alive_persons["relate"] = alive_persons["relate"].astype(int)
+
+    return alive_persons
+
+def get_widows(alive_persons, dead_persons):
+    """Function to get the widows from the mortality model
+
+    Args:
+        alive_persons (pd.DataFrame): DataFrame of alive persons
+        dead_persons (pd.DataFrame): DataFrame of dead persons
+
+    Returns:
+        pd.DataFrame: DataFrame of widowed individuals
+    """
+    alive_persons = alive_persons[["household_id", "MAR"]]
+    widow = alive_persons.reset_index().merge(
+        dead_persons[["household_id"]], how="inner", on="household_id"
+    )
+    widow = widow.set_index("person_id")
+    widow["MAR"] = 3
+    
+    return widow
 
 def rez(group):
     """
@@ -493,54 +487,14 @@ def rez(group):
     return group
 
 
-# TODO refactor this method so it can be cleanly used with any model
-# Might need to add some stuff (update more variables) or have a flag to determine which function is calling this
-def update_households(alive, dead, old_incomes, subtract=True):
-    """
-    Function to update the households characteristics, namely income, num of workers, race, and age of head.
-    """
-    # By default the dead variable represents people dying or leaving a household
-    if subtract:
-        alive = alive.sort_values("relate")
-        dead_aggs = dead.groupby("household_id").agg({"earning": "sum"})
-
-        aggregates = alive.groupby("household_id").agg(
-            {"earning": "sum", "worker": "sum", "race_id": "first", "age": "first"}
-        )
-        # print(list(orca.get_table("households").to_frame().columns))
-        # TODO -- REPLACE THESE OPERATIONS BY PANDAS OPERATIONS
-        orca.get_table("households").update_col(
-            "income", old_incomes.subtract(dead_aggs["earning"], fill_value=0)
-        )
-        orca.get_table("households").update_col("workers", aggregates["worker"])
-        orca.get_table("households").update_col("race_of_head", aggregates["race_id"])
-        orca.get_table("households").update_col("age_of_head", aggregates["age"])
-    # In a certain case, the dead variable actually represents living people being added -- What is this case?
-    else:
-        aggregates = dead.groupby("household_id").agg(
-            {"earning": "sum", "worker": "sum"}
-        )
-        house_df = orca.get_table("households").to_frame(columns=["income", "workers"])
-        # TODO -- REPLACE THESE OPERATIONS WITH PANDAS OPERATIONS
-        orca.get_table("households").update_col(
-            "income", house_df["income"].add(aggregates["earning"], fill_value=0)
-        )
-        orca.get_table("households").update_col(
-            "workers", house_df["workers"].add(aggregates["worker"], fill_value=0)
-        )
-
-    # return households
-
-
-# Function that takes the head's previous role and returns a function
-# that maps roles to new roles based on restructuring
 def produce_map_func(old_role):
     """
     Function that uses the relationship mapping in the
     provided table and returns a function that maps
     new household roles.
     """
-    # old role is the previous number of the person who has now been promoted to head of the household
+    # old role is the previous number of the person who
+    # has now been promoted to head of the household
     sold_role = str(old_role)
  
     def inner(role):
